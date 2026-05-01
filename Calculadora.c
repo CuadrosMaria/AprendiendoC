78a560caf4397e4726b246720775d6ab72a384c7
Andrea <cuadrosmariaandrea@gamil.com>
2026-04-30 23:37:28 -0500

    Commit Crear Archivo y registro numeros

---
+++ Calculadora.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void numeros (float *a, float*b){
+    printf("Ingrese el primer numero:");
+    scanf("%f",a);
+    printf("Ingrese el segundo numero:");
+    scanf("%f",b);
+}
+int main() {
+    return 0;
+}
