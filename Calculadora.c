bbef73e81ef9b7e0660ab32f45632fdb7f70f2d3
Andrea <cuadrosmariaandrea@gamil.com>
2026-04-30 23:41:31 -0500

    Commit Metodo Burbuja

--- Calculadora.c
+++ Calculadora.c
@@ -5,6 +5,14 @@ void numeros (float *a, float*b){
     printf("Ingrese el segundo numero:");
     scanf("%f",b);
 }
+void Reordenar(float *a, float*b){
+    float aux;
+    if (*a>*b){
+        aux = *a;
+        *a = *b;
+        *b = aux;
+    }
+}
 int main() {
     return 0;
 }
